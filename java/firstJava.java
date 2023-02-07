bd0696a2d83ff8a8bff10652acef7bdc8a9a3ebb
Nelson M <nelsondmondragon@gmail.com>
2023-02-07 16:02:19 -0500

    Cafe java

--- java/firstJava.java
+++
@@ -1,7 +0,0 @@
-public class firstJava {
-    public static void main(String[] args) {
-        System.out.println("Mi nombre es ".concat("Nelson Cortes"));
-        System.out.println("Tengo 23 annos de edad");
-        System.out.println("Mi ciudad natal es Cartagena del Chaira, Colombia");
-    }
-}

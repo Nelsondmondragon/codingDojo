6579c76293404ecddb6f0164032c27243408fb05
Nelson M <nelsondmondragon@gmail.com>
2023-02-07 16:17:52 -0500

    logica de negocio

--- firstVSCode/Cortes_Nelson_JS_Jugueteo.js
+++
@@ -1,7 +0,0 @@
-var alturaNino = 63;
-
-function muestraSiElNiñoPuedeSubirALaMontañaRusa() {
-    console.log((alturaNino < 52) ? 'Lo siento, chico. Tal vez el próximo año' : '¡Súbete, chico!');
-}
-
-muestraSiElNiñoPuedeSubirALaMontañaRusa();

5ac779b3ee236e79a9d9ad7912645b3a840b998c
Nelson M <nelsondmondragon@gmail.com>
2023-02-21 17:05:53 -0500

    save

---
+++ automovilconductor/Conductor.java
@@ -0,0 +1,21 @@
+package automovilconductor;
+
+public class Conductor extends Automovil {
+
+    public void conducir() {
+        this.setGasolina(1);
+        System.out.println("Conducir");
+
+    }
+
+    public void propulsores() {
+        this.setGasolina(3);
+        System.out.println("Propulsores");
+    }
+
+    public void llenarGasolina() {
+        this.se
+        System.out.println("Gasolina");
+    }
+
+}
